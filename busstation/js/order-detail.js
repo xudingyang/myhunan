f685c7b102b1cb90d30e8cf227d13322bc3d158f
xdy <xdymail@163.com>
2018-06-05 11:58:41 +0800

    0605

--- busstation/js/order-detail.js
+++ busstation/js/order-detail.js
@@ -1,6 +1,6 @@
 $(function () {
 
-    var state = 2
+    var state = 1
     if (1 === state) {
         // 已支付
         $('.state-had-pay').show()
@@ -12,4 +12,15 @@ $(function () {
         $('.state-had-cancel').show()
         $('.operate-wrapper').hide()
     }
+
+    // 点击退票按钮
+    $('.refund-btn').on('click',function () {
+        if (confirm('确认退票吗？')) {
+            console.log(123);
+        }
+    })
+    // 点击二维码取票
+    $('.get-ticket-btn').on('click',function () {
+        location.href = 'ticket-qrcode.html'
+    })
 })
