33b0bf832cfea4310f04d99b86abb7685d81559e
xdy <xdymail@163.com>
2018-06-05 18:19:16 +0800

    0605

--- busstation/js/user-center.js
+++ busstation/js/user-center.js
@@ -1,19 +1,24 @@
 $(function () {
-    // 点击加油卡
-    $('.jiayouka').on('click',function () {
-        location.href = 'jiayouka.html'
-    })
+
+    // 如果设置了手机号
+    var hadSetPhone = true
+    var phone = '13866668888'
+    if (hadSetPhone) {
+        $('.right-phone').text(phone.replace(/(\d{3})\d{4}(\d{4})/, '$1****$2'))
+    } else {
+        $('.right-phone').text('未绑定')
+    }
     // 点击优惠券
     $('.youhuiquan').on('click',function () {
-        location.href = 'youhuiquan.html'
+        location.href = 'coupon.html'
     })
     // 点击订单
     $('.js-order').on('click',function () {
         location.href = 'order.html'
     })
-    // 点击消费记录
-    $('#xfjl').on('click',function () {
-        location.href = 'user-center-xfjl.html'
+    // 点击常用乘客
+    $('#cyck').on('click',function () {
+        location.href = 'passengers.html'
     })
     // 点击个人资料
     $('#grzl').on('click',function () {
