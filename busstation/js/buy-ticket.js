7fd4bca034e2258c5769956a57b4a5ce454c2655
xdy <xdymail@163.com>
2018-06-08 14:23:40 +0800

    0608

---
+++ busstation/js/buy-ticket.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('.js-start-station').click(function () {
+        location.href = 'buy-ticket-start-station.html'
+    })
+    $('.js-end-station').click(function () {
+        location.href = 'buy-ticket-end-station.html'
+    })
+})
