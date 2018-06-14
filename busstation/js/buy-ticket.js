3b0472b16bbcef7ac1781d8e38c7e37d818d9f3e
xdy <xdymail@163.com>
2018-06-14 11:54:00 +0800

    0614

--- busstation/js/buy-ticket.js
+++ busstation/js/buy-ticket.js
@@ -11,12 +11,21 @@ $(function () {
     $('.js-trip-date').click(function () {
         $('.date-modal-wrapper').show()
     })
+    // 点击选班次按钮
+    $('.js-choose-bus').click(function () {
+        location.href = 'buy-ticket-choose-bus.html'
+    })
     $('.date-modal').on('click',function () {
         $('.date-modal-wrapper').hide()
     })
     $('#schedule-box').on('click',function (e) {
         e.stopPropagation()
     })
+
+    var weeks = ['星期天', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
+    var tmpDate = new Date()
+    $('#date_str').text(tmpDate.getFullYear() + '-' + (tmpDate.getMonth() + 1) + '-' + tmpDate.getDate())
+    $('#week_str').text(weeks[tmpDate.getDay()])
     // 日期控件
     var mySchedule = new Schedule({
         el: '#schedule-box',
@@ -25,7 +34,6 @@ $(function () {
             console.log()
             var dateStr = y + '-' + m + '-' + d
             var date = new Date(dateStr)
-            var weeks = ['星期天', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
             $('#date_str').text(dateStr)
             $('#week_str').text(weeks[date.getDay()])
             $('.date-modal-wrapper').hide()
