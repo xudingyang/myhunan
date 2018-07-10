8673137cde8a8a1776af5243000b58eb3eec4b79
xdy <xdymail@163.com>
2018-07-10 14:40:05 +0800

    0710

--- gasstation/js/company-user.js
+++ gasstation/js/company-user.js
@@ -1,3 +1,8 @@
 $(function () {
-    
+    $('.add-btn-wrapper').on('click',function () {
+        location.href = 'company-user-edit.html'
+    })
+    $('li').click(function () {
+        location.href = 'company-user-detail.html'
+    })
 })
