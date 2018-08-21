7f3bab3c5752a3251b03186abe23fc72e6b2d9a6
xdy <xdymail@163.com>
2018-08-21 18:24:47 +0800

    0821

--- 奶茶店/pages/choose_store/choose_store.js
+++ 奶茶店/pages/choose_store/choose_store.js
@@ -1,66 +1,116 @@
 // pages/choose_store/choose_store.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    inputHasText: false,
+    array: ['长沙市', '岳阳市', '湘潭市', '株洲市'],
+    index: 0,
+    latitude: 23.099994,
+    longitude: 113.324520,
+    markers: [{
+      id: 1,
+      latitude: 23.099994,
+      longitude: 113.324520,
+      name: '极品哈士奇',
+      iconPath: '/images/dog1.jpg',
+      width: 40,
+      height: 40,
+      callout: {
+        content: '哈士奇奶茶店',
+        color: '#000',
+        fontSize: 20,
+        padding: 6,
+        borderRadius: 4        
+      }
+    }, {
+      id: 2,
+      latitude: 23.099994,
+      longitude: 113.328620,
+      name: '毛毛奶茶',
+      iconPath: '/images/location.png',
+      callout: {
+        content: '首都奶茶',
+        color: '#000',
+        fontSize: 20,
+        padding: 6,
+        borderRadius: 4
+      }
+    }, {
+      id: 3,
+      latitude: 23.099994,
+      longitude: 113.332620,
+      name: '猫咪奶茶店',
+      iconPath: '/images/cat_avatar.jpg',
+      width: 40,
+      height: 40,
+      callout: {
+        content: '猫咪奶茶店',
+        color: '#000',
+        fontSize: 20,
+        padding: 6,
+        borderRadius: 4        
+      }
+    }]
   },
-
+  bindPickerChange: function(e) {
+    console.log(this.data.array[e.detail.value])
+    this.setData({
+      index: e.detail.value
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   }
 })
