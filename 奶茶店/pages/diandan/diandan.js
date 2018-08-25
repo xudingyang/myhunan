44bc67797533c6b65564a0a096b7fc37d3356b72
xdy <xdymail@163.com>
2018-08-25 17:19:46 +0800

    0825

--- 奶茶店/pages/diandan/diandan.js
+++ 奶茶店/pages/diandan/diandan.js
@@ -12,33 +12,370 @@ Page({
         icon: '/images/tea/tea.jpg',
         introduction: '广东的奶茶，原味的哦，很好喝的奶茶，哈哈哈哈是范德萨的法撒旦发送到范德萨发送到阿斯蒂芬阿诗丹顿发大水发地方',
         price: '20',
-        discount_price: '15'
+        count: 8,
+        size: [{
+            theme: '味道',
+            content: [{
+                name: '苦味',
+                selected: true
+              },
+              {
+                name: '甜味',
+                selected: false
+              }
+            ]
+          },
+          {
+            theme: '冰块',
+            content: [{
+                name: '加冰',
+                selected: false
+              },
+              {
+                name: '不加冰',
+                selected: false
+              },
+              {
+                name: '多加点冰',
+                selected: false
+              }
+            ]
+          },
+          {
+            theme: '温度',
+            content: [{
+                name: '常温',
+                selected: false
+              },
+              {
+                name: '加热',
+                selected: false
+              },
+              {
+                name: '冰冻',
+                selected: false
+              },
+              {
+                name: '多冰',
+                selected: false
+              },
+              {
+                name: '去冰',
+                selected: false
+              },
+              {
+                name: '少冰',
+                selected: false
+              },
+              {
+                name: '少冰',
+                selected: false
+              },
+            ]
+          }
+        ]
+      }, {
+        name: '广东好喝的原味奶茶',
+        icon: '/images/tea/tea.jpg',
+        introduction: '广东的奶茶，原味的哦，很好喝的奶茶，哈哈哈哈是范德萨的法撒旦发送到范德萨发送到阿斯蒂芬阿诗丹顿发大水发地方',
+        price: '20',
+        count: 8,
+        size: []
       }]
     }, {
-        category_tx1: '韩式原味',
-        category_tx2: '奶茶苦',
-        categoryImage: '/images/tea/putao.jpg',
-        selected: false,
-        teaList: [{
-          name: '广东好喝的原味奶茶',
-          icon: '/images/tea/tea.jpg',
-          introduction: '广东的奶茶，原味的哦，很好喝的奶茶，哈哈哈哈是范德萨的法撒旦发送到范德萨发送到阿斯蒂芬阿诗丹顿发大水发地方',
-          price: '20',
-          discount_price: '15'
-        }]
-      }, {
-        category_tx1: '韩式奶茶',
-        category_tx2: '',
-        categoryImage: '/images/tea/putao.jpg',
-        selected: false,
-        teaList: [{
-          name: '广东好喝的原味奶茶',
-          icon: '/images/tea/tea.jpg',
-          introduction: '广东的奶茶，原味的哦，很好喝的奶茶，哈哈哈哈是范德萨的法撒旦发送到范德萨发送到阿斯蒂芬阿诗丹顿发大水发地方',
-          price: '20',
-          discount_price: '15'
-        }]
+      category_tx1: '草莓系列',
+      category_tx2: '奶茶',
+      categoryImage: '',
+      selected: false,
+      teaList: [{
+        name: '广东好喝的原味奶茶',
+        icon: '/images/tea/tea.jpg',
+        introduction: '广东的奶茶，原味的哦，很好喝的奶茶，哈哈哈哈是范德萨的法撒旦发送到范德萨发送到阿斯蒂芬阿诗丹顿发大水发地方',
+        price: '20',
+        count: 8,
+        size: [{
+            theme: '味道',
+            content: [{
+                name: '苦味',
+                selected: true
+              },
+              {
+                name: '甜味',
+                selected: false
+              }
+            ]
+          },
+          {
+            theme: '冰块',
+            content: [{
+                name: '加冰',
+                selected: false
+              },
+              {
+                name: '不加冰',
+                selected: false
+              },
+              {
+                name: '多加点冰',
+                selected: false
+              }
+            ]
+          },
+          {
+            theme: '温度',
+            content: [{
+                name: '常温',
+                selected: false
+              },
+              {
+                name: '加热',
+                selected: false
+              },
+              {
+                name: '冰冻',
+                selected: false
+              },
+              {
+                name: '多冰',
+                selected: false
+              },
+              {
+                name: '去冰',
+                selected: false
+              },
+              {
+                name: '少冰',
+                selected: false
+              },
+              {
+                name: '少冰',
+                selected: false
+              },
+            ]
+          }
+        ]
       }]
+    }, {
+      category_tx1: '韩式原味',
+      category_tx2: '苦奶茶',
+      categoryImage: '/images/tea/putao.jpg',
+      selected: false,
+      teaList: [{
+        name: '广东好喝的原味奶茶',
+        icon: '/images/tea/tea.jpg',
+        introduction: '广东的奶茶，原味的哦，很好喝的奶茶，哈哈哈哈是范德萨的法撒旦发送到范德萨发送到阿斯蒂芬阿诗丹顿发大水发地方',
+        price: '20',
+        count: 8,
+        size: [{
+            theme: '味道',
+            content: [{
+                name: '苦味',
+                selected: true
+              },
+              {
+                name: '甜味',
+                selected: false
+              }
+            ]
+          },
+          {
+            theme: '冰块',
+            content: [{
+                name: '加冰',
+                selected: false
+              },
+              {
+                name: '不加冰',
+                selected: false
+              },
+              {
+                name: '多加点冰',
+                selected: false
+              }
+            ]
+          },
+          {
+            theme: '温度',
+            content: [{
+                name: '常温',
+                selected: false
+              },
+              {
+                name: '加热',
+                selected: false
+              },
+              {
+                name: '冰冻',
+                selected: false
+              },
+              {
+                name: '多冰',
+                selected: false
+              },
+              {
+                name: '去冰',
+                selected: false
+              },
+              {
+                name: '少冰',
+                selected: false
+              },
+              {
+                name: '少冰',
+                selected: false
+              },
+            ]
+          }
+        ]
+      }, {
+        name: '广东好喝的原味奶茶',
+        icon: '/images/tea/tea.jpg',
+        introduction: '广东的奶茶，原味的哦，很好喝的奶茶，哈哈哈哈是范德萨的法撒旦发送到范德萨发送到阿斯蒂芬阿诗丹顿发大水发地方',
+        price: '20',
+        count: 8,
+        size: [{
+            theme: '味道',
+            content: [{
+                name: '苦味',
+                selected: true
+              },
+              {
+                name: '甜味',
+                selected: false
+              }
+            ]
+          },
+          {
+            theme: '冰块',
+            content: [{
+                name: '加冰',
+                selected: false
+              },
+              {
+                name: '不加冰',
+                selected: false
+              },
+              {
+                name: '多加点冰',
+                selected: false
+              }
+            ]
+          },
+          {
+            theme: '温度',
+            content: [{
+                name: '常温',
+                selected: false
+              },
+              {
+                name: '加热',
+                selected: false
+              },
+              {
+                name: '冰冻',
+                selected: false
+              },
+              {
+                name: '多冰',
+                selected: false
+              },
+              {
+                name: '去冰',
+                selected: false
+              },
+              {
+                name: '少冰',
+                selected: false
+              },
+              {
+                name: '少冰',
+                selected: false
+              },
+            ]
+          }
+        ]
+      }, {
+        name: '广东好喝的原味奶茶',
+        icon: '/images/tea/tea.jpg',
+        introduction: '广东的奶茶，原味的哦，很好喝的奶茶，哈哈哈哈是范德萨的法撒旦发送到范德萨发送到阿斯蒂芬阿诗丹顿发大水发地方',
+        price: '20',
+        count: 8,
+        size: [{
+            theme: '味道',
+            content: [{
+                name: '苦味',
+                selected: true
+              },
+              {
+                name: '甜味',
+                selected: false
+              }
+            ]
+          },
+          {
+            theme: '冰块',
+            content: [{
+                name: '加冰',
+                selected: false
+              },
+              {
+                name: '不加冰',
+                selected: false
+              },
+              {
+                name: '多加点冰',
+                selected: false
+              }
+            ]
+          },
+          {
+            theme: '温度',
+            content: [{
+                name: '常温',
+                selected: false
+              },
+              {
+                name: '加热',
+                selected: false
+              },
+              {
+                name: '冰冻',
+                selected: false
+              },
+              {
+                name: '多冰',
+                selected: false
+              },
+              {
+                name: '去冰',
+                selected: false
+              },
+              {
+                name: '少冰',
+                selected: false
+              },
+              {
+                name: '少冰',
+                selected: false
+              },
+            ]
+          }
+        ]
+      }]
+    }, {
+      category_tx1: '韩式奶茶',
+      category_tx2: '',
+      categoryImage: '/images/tea/putao.jpg',
+      selected: false,
+      teaList: [{
+        name: '广东好喝的原味奶茶',
+        icon: '/images/tea/tea.jpg',
+        introduction: '广东的奶茶，原味的哦，很好喝的奶茶，哈哈哈哈是范德萨的法撒旦发送到范德萨发送到阿斯蒂芬阿诗丹顿发大水发地方',
+        price: '20',
+        count: 8,
+        size: []
+      }]
+    }]
   },
   onLoad: function(options) {
     if (!this.data.localHasStore) {
